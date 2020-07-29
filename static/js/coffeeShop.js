6bffb23cc9dcf1ed2f6b73645e35d5da0e6914ba
Maily Ramos <maily.ramos1993@gmail.com>
2020-07-29 23:25:23 +0000

    Finished Lab

--- static/js/coffeeShop.js
+++ static/js/coffeeShop.js
@@ -38,10 +38,19 @@ const setProgressAndStatus = (progressVal, statusMsg) => {
 //
 // Add your event handlers below.
 //
+
+
 $('.add-to-order').on('click', () => {
-addItemToCart('coffee');
-incrementCartTotal('1.50');
+  addItemToCart('Coffee');
+  incrementCartTotal(1.50);
+});
+
+$('#place-order').on('click', () => {
+  incrementCoffeeSold($('#cart-items').children().length);
+ 
+ resetCart();
 });
 
 
-s
+
+
