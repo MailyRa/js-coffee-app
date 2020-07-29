d0ea2981b03dce859cb1a2163cde8b8e811e37c7
Maily Ramos <maily.ramos1993@gmail.com>
2020-07-29 23:08:47 +0000

    Add to Order

--- static/js/coffeeShop.js
+++ static/js/coffeeShop.js
@@ -38,3 +38,10 @@ const setProgressAndStatus = (progressVal, statusMsg) => {
 //
 // Add your event handlers below.
 //
+$('.add-to-order').on('click', () => {
+addItemToCart('coffee');
+incrementCartTotal('1.50');
+});
+
+
+s
